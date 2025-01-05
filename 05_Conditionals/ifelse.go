25213c43267e4e928e7223417c7f08b47cb416f8
Abhinay Jangde <64852930+abhinayjangde@users.noreply.github.com>
2025-01-05 14:29:56 +0000

    if else

---
+++ 05_Conditionals/ifelse.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// age := 22
+
+	// if age >= 18 {
+	// 	fmt.Println("Age is greater than 18")
+	// } else {
+	// 	fmt.Println("Age is less than 18")
+	// }
+
+	// else-if
+
+	// age := 22
+	// if age >= 18 {
+	// 	fmt.Println("person is adult")
+	// } else if age >= 12 {
+	// 	fmt.Println("person is teenager")
+	// } else {
+	// 	fmt.Println("person is a kid")
+	// }
+
+	// var role string = "admin"
+	// var hasPermissions bool = false
+
+	// if role == "admin" || hasPermissions {
+	// 	fmt.Println("yes")
+	// }
+
+	// if age := 15; age >= 18 {
+	// 	fmt.Println("person is an adult ", age)
+	// } else if age >= 12 {
+	// 	fmt.Println("person is teenager ", age)
+	// }
+
+	// go does not have ternary, you will have to use normal if else.
+}
