f59be08d338ba250765f05a3b5d885eda3b9a1aa
Abhinay Jangde <64852930+abhinayjangde@users.noreply.github.com>
2025-01-06 17:36:13 +0000

    main.go is for revision

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// const PI = 3.14
+	// PI = 21
+	// age := 22
+	// fmt.Println("Hello World!", age)
+	// fmt.Println("value of pi is ", PI)
+
+	var (
+		name string = "Abhi"
+		age int = 22
+		email string = "abhi@gmail.com"
+	)
+	fmt.Println(name,email,age)
+}
