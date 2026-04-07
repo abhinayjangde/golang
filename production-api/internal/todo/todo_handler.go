b8039a41ea62f25cfaac31e9f3a5f5677db94de9
Abhinay Jangde <64852930+abhinayjangde@users.noreply.github.com>
2026-04-07 10:44:43 +0000

    path, get sinlge and delete route is done

--- production-api/internal/todo/todo_handler.go
+++ production-api/internal/todo/todo_handler.go
@@ -7,3 +7,15 @@ func CreateTodo(todo Todo) error {
 func GetTodos() ([]Todo, error) {
 	return GetAll()
 }
+
+func GetTodoById(id string) (Todo, error) {
+	return GetById(id)
+}
+
+func UpdateTodoById(id string, update Todo) error {
+	return UpdateTodo(id, update)
+}
+
+func DeleteTodoById(id string) error {
+	return DeleteTodo(id)
+}
--- production-api/internal/todo/todo_routes.go
+++ production-api/internal/todo/todo_routes.go
@@ -5,4 +5,8 @@ import "github.com/gin-gonic/gin"
 func TodoRoutes(router *gin.Engine) {
 	router.POST("/todos", CreateTodoController)
 	router.GET("/todos", GetTodosController)
+
+	router.GET("/todos/:id", GetTodoByIDController)
+	router.PUT("/todos/:id", UpdateTodoController)
+	router.DELETE("/todos/:id", DeleteTodoController)
 }
