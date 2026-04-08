83ae16f48d747e75056ba447b4ca1b22d1a5a961
Abhinay Jangde <64852930+abhinayjangde@users.noreply.github.com>
2026-04-08 07:16:28 +0000

    graceful shutdown

--- 51-Students-API/cmd/main.go
+++ 51-Students-API/cmd/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/abhinayjangde/student/internal/config"
 )
@@ -29,10 +34,27 @@ func main() {
 		Handler: router,
 	}
 
-	fmt.Printf("Server started at %s", cfg.HTTPServer.Addr)
-	err := server.ListenAndServe()
+	slog.Info("Server started at ", slog.String("address", cfg.HTTPServer.Addr))
 
-	if err != nil {
-		log.Fatalf("Failed to start server")
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatalf("Failed to start server")
+		}
+	}()
+
+	<-done // wait for interrupt signal. it will block the main goroutine until we receive an interrupt signal
+
+	slog.Info("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
 	}
+
+	slog.Info("Server gracefully stopped")
 }
--- 51-Students-API/docs.md
+++ 51-Students-API/docs.md
@@ -1,3 +1,4 @@
 go get -u github.com/ilyakaznacheev/cleanenv
 https://github.com/ilyakaznacheev/cleanenv
 
+https://youtu.be/OGhQhFKvMiM?si=fCNo9VsSYcEz3JpQ 
