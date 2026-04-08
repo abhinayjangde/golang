00c829919447f72f7c4fb24b113dcc71b62f8721
Abhinay Jangde <abhinayjangde@gmail.com>
2026-04-08 12:27:23 +0530

    running server

--- 51-Students-API/cmd/main.go
+++ 51-Students-API/cmd/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/abhinayjangde/student/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	// load config
+	cfg := config.MustLoad()
+
+	// database setup
+
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("server is running..."))
+	})
+
+	// setup server
+
+	server := http.Server{
+		Addr:    cfg.HTTPServer.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("Server started at %s", cfg.HTTPServer.Addr)
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatalf("Failed to start server")
+	}
 }
