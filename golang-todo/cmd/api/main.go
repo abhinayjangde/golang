302684a8dc956e83480af9f4c347e53b061c4c1d
Abhinay Jangde <64852930+abhinayjangde@users.noreply.github.com>
2026-04-25 05:14:35 +0000

    add postgres db in docker-compose file

--- golang-todo/cmd/api/main.go
+++ golang-todo/cmd/api/main.go
@@ -9,13 +9,16 @@ import (
 
 func main() {
 	var router *gin.Engine = gin.Default()
+
 	router.SetTrustedProxies(nil)
-	router.GET("/", func(c *gin.Context) {
+
+	router.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message":   "api is working",
 			"status":    "success",
 			"timestamp": time.Now().UTC(),
 		})
 	})
+
 	router.Run(":8080")
 }
