7c43541817f92d0f16a0e7f88ca35a824bfa7b16
Abhinay Jangde <64852930+abhinayjangde@users.noreply.github.com>
2026-04-25 05:08:28 +0000

    initialize project structure with main API, configuration, and environment files

---
+++ golang-todo/cmd/api/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	var router *gin.Engine = gin.Default()
+	router.SetTrustedProxies(nil)
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message":   "api is working",
+			"status":    "success",
+			"timestamp": time.Now().UTC(),
+		})
+	})
+	router.Run(":8080")
+}
