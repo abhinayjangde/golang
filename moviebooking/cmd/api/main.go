a65f9422c73a706a72f7a84f1908670523d95ef8
Abhinay Jangde <abhinayjangde@gmail.com>
2026-08-07 11:49:02 +0530

    add ping route

---
+++ moviebooking/cmd/api/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		// Return JSON response
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run(":8080")
+}
