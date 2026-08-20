4a843eb13d6b1fdce243a00954fa70401e4d773a
Abhinay Jangde <abhinayjangde@gmail.com>
2026-08-20 19:47:44 +0530

    slice in go

--- main.go
+++ main.go
@@ -1,39 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
-
-func print10(n int) int {
-	if n == 11 {
-		return 0
-	}
-	fmt.Println(n)
-	return print10(n + 1)
-}
-func worker(id int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println("worker", id, "started")
-	time.Sleep(time.Second)
-	fmt.Println("worker", id, "done")
-}
-
-func nums(ch chan<- int) {
-	for i := range 5 {
-		ch <- i
-	}
-	close(ch)
-}
+import "fmt"
 
 func main() {
-	ch := make(chan int, 5)
+	slice := []int{1, 2, 3}
+	// fmt.Println(slice)
+	slice = append(slice, 4, 5)
+	fmt.Println(slice)
 
-	nums(ch)
-	for n := range ch {
-		fmt.Println(n)
-	}
-
-	print10(1)
 }
