7a3dd2b186ff00ac7ec079cc50dd20fe3a1a5f88
Abhinay Jangde <abhinayjangde@gmail.com>
2026-08-21 16:17:58 +0530

    read directory

--- main.go
+++ main.go
@@ -7,10 +7,28 @@ import (
 
 func main() {
 
-	data, err := os.ReadFile("README.md")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(data))
+	// data, err := os.ReadFile("README.md")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(string(data))
 
+	// file, err := os.Create("hello.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// defer file.Close()
+	// file.WriteString("test")
+	dir, err := os.Open(".")
+	if err != nil {
+		return
+	}
+	defer dir.Close()
+
+	fileInfors, err := dir.ReadDir(3)
+
+	for _, f := range fileInfors {
+		fmt.Println(f.Name())
+	}
 }
