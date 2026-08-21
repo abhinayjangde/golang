73f830374f3001ce4221a3942006a9ccd54ec551
Abhinay Jangde <abhinayjangde@gmail.com>
2026-08-21 22:05:56 +0530

    gen ai opneai

---
+++ gen/cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/openai/openai-go/v3"
+	"github.com/openai/openai-go/v3/option"
+	"github.com/openai/openai-go/v3/responses"
+)
+
+func main() {
+	godotenv.Load()
+	client := openai.NewClient(
+		option.WithAPIKey(os.Getenv("GROQ_API_KEY")),
+		option.WithBaseURL("https://api.groq.com/openai/v1"),
+	)
+
+	response, err := client.Responses.New(context.Background(), responses.ResponseNewParams{
+		Model: "groq/compound",
+		Input: responses.ResponseNewParamsInputUnion{
+			OfString: openai.String("Write a short bedtime story about a unicorn."),
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(response.OutputText())
+}
---
+++ gen/go.mod
@@ -0,0 +1,13 @@
+module github.com/abhinayjangde/gen
+
+go 1.27.0
+
+require github.com/openai/openai-go/v3 v3.52.0
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/tidwall/gjson v1.19.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
+)
