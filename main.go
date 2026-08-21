1cb849211bdd4cbe8d18c96eb7a50aff19d492ce
Abhinay Jangde <abhinayjangde@gmail.com>
2026-08-21 21:29:47 +0530

    concurrency in go

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
-func main() {
-	var input string
-	fmt.Scanln(&input)
-	fmt.Println("your input is ", input)
+func f(n int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("done ", n)
 
+	time.Sleep(time.Millisecond * 50)
+}
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add((10))
+	for i := range 10 {
+		f(i, &wg)
+	}
+	wg.Wait()
 }
