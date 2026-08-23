6c5d34b81b067cee3e1013ad5aba05dcabd35c36
Abhinay Jangde <abhinayjangde@gmail.com>
2026-08-23 17:19:41 +0530

    move corsMiddleware to middlewares package

--- olxapi/cmd/api/main.go
+++ olxapi/cmd/api/main.go
@@ -8,20 +8,9 @@ import (
 	"github.com/abhinayjangde/olxapi/internal/config"
 	"github.com/abhinayjangde/olxapi/internal/db"
 	"github.com/abhinayjangde/olxapi/internal/handlers"
+	"github.com/abhinayjangde/olxapi/internal/middlewares"
 )
 
-func corsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
 func main() {
 	cfg := config.MustLoad()
 	db, err := db.Connect(cfg.DatabaseUrl)
@@ -29,7 +18,7 @@ func main() {
 		log.Fatalf("main.db.connect: %v", err)
 	}
 	mux := http.NewServeMux()
-	wrappedMux := corsMiddleware(mux)
+	wrappedMux := middlewares.CorsMiddleware(mux) // for fixing cors policy
 
 	log.Println("database connected")
 	log.Printf("starting server on port %s", cfg.Port)
