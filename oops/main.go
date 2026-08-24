91e9ddc91f80c9045c5bbd6c43744c9b3a3cc427
Abhinay Jangde <abhinayjangde@gmail.com>
2026-08-24 16:01:59 +0530

    methods in oops

--- oops/main.go
+++ oops/main.go
@@ -2,26 +2,6 @@ package main
 
 import "fmt"
 
-type Person struct {
-	Name string
-	Age  int
-}
-
-func (p Person) Show() {
-	fmt.Println(p.Name)
-	fmt.Println(p.Age)
-}
-
-func (p *Person) Birthday() {
-	fmt.Println("Happy birthday")
-	p.Age++
-}
 func main() {
-	person := Person{
-		Name: "Abhi",
-		Age:  24,
-	}
-	person.Show()
-	person.Birthday()
-	person.Show()
+	fmt.Println("hello")
 }
