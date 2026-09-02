2a0ea3ed06b82f6a708689e1da8d01b077950472
Abhinay Jangde <abhinayjangde@gmail.com>
2026-09-02 08:40:43 +0530

    add two numbers

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var a, b, c byte
-
+	var a, b, c int
+	fmt.Print("enter first number: ")
 	fmt.Scan(&a)
+	fmt.Print("enter first number: ")
 	fmt.Scan(&b)
 
 	c = a + b
-	fmt.Println(c)
+	fmt.Println("sum = ", c)
 }
