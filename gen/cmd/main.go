596b293e4b18dc327b3f0f9091e98c2fb66757fd
Abhinay Jangde <abhinayjangde@gmail.com>
2026-09-03 15:31:43 +0530

    cli sarvam api chat

--- gen/cmd/main.go
+++ gen/cmd/main.go
@@ -5,28 +5,46 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/abhinayjangde/sarvam"
 	"github.com/joho/godotenv"
-	"github.com/openai/openai-go/v3"
-	"github.com/openai/openai-go/v3/option"
-	"github.com/openai/openai-go/v3/responses"
 )
 
 func main() {
 	godotenv.Load()
-	client := openai.NewClient(
-		option.WithAPIKey(os.Getenv("GROQ_API_KEY")),
-		option.WithBaseURL("https://api.groq.com/openai/v1"),
-	)
 
-	response, err := client.Responses.New(context.Background(), responses.ResponseNewParams{
-		Model: "groq/compound",
-		Input: responses.ResponseNewParamsInputUnion{
-			OfString: openai.String("Write a short bedtime story about a unicorn."),
-		},
-	})
-	if err != nil {
-		panic(err)
+	client := sarvam.NewClient(os.Getenv("SARVAM_API_KEY"))
+	var messages []sarvam.Message
+
+	for true {
+		var query string
+		fmt.Print("> ")
+		fmt.Scanln(&query)
+
+		if query == "exit" {
+			break
+		}
+		messages = append(messages, sarvam.Message{
+			Role:    "user",
+			Content: query,
+		})
+		resp, err := client.Chat.Completions.Create(
+			context.Background(),
+			sarvam.ChatCompletionRequest{
+				Model:    sarvam.ModelSarvam105BConversations,
+				Messages: messages,
+			},
+		)
+
+		if err != nil {
+			panic(err)
+		}
+
+		messages = append(messages, sarvam.Message{
+			Role:    "assistant",
+			Content: resp.Choices[0].Message.Content,
+		})
+
+		fmt.Println("> " + resp.Choices[0].Message.Content)
 	}
 
-	fmt.Println(response.OutputText())
 }
--- gen/go.mod
+++ gen/go.mod
@@ -2,12 +2,9 @@ module github.com/abhinayjangde/gen
 
 go 1.27.0
 
-require github.com/openai/openai-go/v3 v3.52.0
-
 require (
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/tidwall/gjson v1.19.0 // indirect
-	github.com/tidwall/match v1.1.1 // indirect
-	github.com/tidwall/pretty v1.2.1 // indirect
-	github.com/tidwall/sjson v1.2.5 // indirect
+	github.com/abhinayjangde/slugify v0.1.0
+	github.com/joho/godotenv v1.5.1
 )
+
+require github.com/abhinayjangde/sarvam v0.1.0 // indirect
