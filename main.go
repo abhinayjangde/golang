6a37965d857b510a9495f3cce1893d7db3a631f3
Abhinay Jangde <abhinayjangde@gmail.com>
2026-09-19 20:39:14 +0530

    setter and getter

--- main.go
+++ main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import "fmt"
+
+type Order struct {
+	owner  string
+	amount int64
+}
+
+func (o Order) Owner() string {
+	return o.owner
+}
+
+func (o *Order) setOwner(owner string) {
+	o.owner = owner
+}
+func main() {
+	o := &Order{}
+
+	o.setOwner("abhi")
+	fmt.Println(o.Owner())
+}
