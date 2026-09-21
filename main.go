56acdc04592518e0482839ef719e30f3d6083adc
Abhinay Jangde <abhinayjangde@gmail.com>
2026-09-21 13:11:05 +0530

    made this to a package

--- main.go
+++ main.go
@@ -2,21 +2,6 @@ package main
 
 import "fmt"
 
-type Order struct {
-	owner  string
-	amount int64
-}
-
-func (o Order) Owner() string {
-	return o.owner
-}
-
-func (o *Order) setOwner(owner string) {
-	o.owner = owner
-}
 func main() {
-	o := &Order{}
-
-	o.setOwner("abhi")
-	fmt.Println(o.Owner())
+	fmt.Printf("%s is my name.", "Abhi")
 }
