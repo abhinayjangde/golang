fd5a7c9f2da26e04ec8c35c5742198cfd079595e
Abhinay Jangde <abhinayjangde@gmail.com>
2026-09-22 10:20:47 +0530

    local changes

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type person struct {
+	FirstName  string
+	MiddleName *string
+	LastName   string
+}
+
+func makePointer[T any](t T) *T {
+	return &t
+}
 func main() {
-	fmt.Printf("%s is my name.", "Abhi")
+	x := 10
+	y := x
+	y = 20
+
+	fmt.Println(x, y)
 }
