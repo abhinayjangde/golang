9e23d689237f99e5e1132119903577c9641c5d15
Abhinay Jangde <abhinayjangde@gmail.com>
2026-09-22 18:53:22 +0530

    using slog to response.go

--- ecom/internal/httpx/response.go
+++ ecom/internal/httpx/response.go
@@ -2,15 +2,16 @@ package httpx
 
 import (
 	"encoding/json"
-	"log"
+	"log/slog"
 	"net/http"
 )
 
 func WriteJSON(w http.ResponseWriter, status int, data any) {
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 
 	if err := json.NewEncoder(w).Encode(data); err != nil {
-		log.Printf("json.Encode: %v", err)
+		slog.Error("json.Encode", "err", err)
 	}
 }
