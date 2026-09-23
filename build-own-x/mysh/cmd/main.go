ef211b6c6e6a753f2d415ed9d69210c4f7c26af7
Abhinay Jangde <abhinayjangde@gmail.com>
2026-09-23 19:06:27 +0530

    add git docker ls pwd cd go commands

--- build-own-x/mysh/cmd/main.go
+++ build-own-x/mysh/cmd/main.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"slices"
+	"os/exec"
 	"strings"
 )
 
@@ -17,18 +17,80 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
-		// command = command[:len(command)-2]
+
 		command = strings.TrimSpace(command)
-		builtins := []string{"exit", "echo", "type"}
+		builtins := map[string]string{
+			"exit":   "exit",
+			"echo":   "echo",
+			"type":   "type",
+			"ls":     "Get-ChildItem",
+			"pwd":    "Get-Location",
+			"cd":     "Set-Location",
+			"go":     "go",
+			"git":    "git",
+			"docker": "docker",
+		}
 
 		if command == "exit" {
 			break
 		} else if strings.HasPrefix(command, "echo ") {
 			fmt.Println(command[5:])
+		} else if strings.HasPrefix(command, "ls") {
+			dirs, err := os.ReadDir(".")
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error reading directory:", err)
+				os.Exit(1)
+			}
+			for _, dir := range dirs {
+				fmt.Println(dir.Name())
+			}
+
+		} else if strings.HasPrefix(command, "pwd") {
+			location, err := os.Getwd()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error getting current directory:", err)
+				os.Exit(1)
+			}
+			fmt.Println(location)
+
+		} else if strings.HasPrefix(command, "git") {
+			args := strings.Fields(command)
+			cmd := exec.Command(builtins["git"], args[1:]...)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			cmd.Stdin = os.Stdin
+			if err := cmd.Run(); err != nil {
+				fmt.Fprintln(os.Stderr, "git error:", err)
+			}
+		} else if strings.HasPrefix(command, "docker") {
+			args := strings.Fields(command)
+			cmd := exec.Command(builtins["docker"], args[1:]...)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			cmd.Stdin = os.Stdin
+			if err := cmd.Run(); err != nil {
+				fmt.Fprintln(os.Stderr, "docker error:", err)
+			}
+		} else if strings.HasPrefix(command, "cd ") {
+			dir := command[3:]
+			err := os.Chdir(dir)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error changing directory:", err)
+				os.Exit(1)
+			}
+
 		} else if strings.HasPrefix(command, "type ") {
 			command = command[5:]
-			if slices.Contains(builtins, command) {
-				fmt.Println(command + " is a shell builtin")
+			if builtins[command] != "" {
+
+				path, err := exec.LookPath(builtins[command])
+
+				if err != nil {
+					fmt.Fprintln(os.Stderr, "Error looking up command:", err)
+					os.Exit(1)
+				}
+				fmt.Println(command+" is ", path)
+				// fmt.Printf("fortune is available at %s\n", path)
 			} else {
 				fmt.Println(command + ": not found")
 			}
