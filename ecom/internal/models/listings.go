8b777a8cdf911a6cb21b0f95d30b1c98810bc9f8
Abhinay Jangde <abhinayjangde@gmail.com>
2026-09-23 08:31:27 +0530

    update listing model

--- ecom/internal/models/listings.go
+++ ecom/internal/models/listings.go
@@ -3,10 +3,19 @@ package models
 import "time"
 
 type Listings struct {
-	ID          string    `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Price       int64     `json:"price"`
-	City        string    `json:"city"`
-	CreatedAt   time.Time `json:"created_at"`
+	ID          string         `json:"id"`
+	Title       string         `json:"title"`
+	Description string         `json:"description"`
+	Price       int64          `json:"price"`
+	City        string         `json:"city"`
+	CreatedAt   time.Time      `json:"created_at"`
+	Images      []ListingImage `json:"images"`
+}
+
+type ListingImage struct {
+	ID          string `json:"id"`
+	URL         string `json:"url"`
+	ContentType string `json:"content_type,omitempty"`
+	Width       int    `json:"width,omitempty"`
+	Height      int    `json:"height,omitempty"`
 }
